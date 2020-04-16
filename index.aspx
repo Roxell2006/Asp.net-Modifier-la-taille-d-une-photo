50bcb9b470c6d1ce16557332eb8e80657fdfff84
Romuald Hansen <romualdhansen2006@gmail.com>
2020-04-16 17:08:15 +0200

    Ajoutez des fichiers projet.

---
+++ index.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Photo_Resize.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Photo Resize</title>
+</head>
+<body>
+    <form id="form1" runat="server" method="post" enctype="multipart/form-data">
+        <div>
+            <asp:FileUpload ID="upFile" accept=".jpg, .jpeg, .png, .gif" runat="server" /><br />
+            <p>Largeur: <asp:TextBox ID="Width"  Text="640" runat="server"></asp:TextBox></p>
+            <p>Hauteur: <asp:TextBox ID="Height" Text="480" runat="server"></asp:TextBox></p><br />
+            <asp:Button style="width: 150px; height:30px; margin-left: 25px;" ID="btnAdd" Text="ReSize" OnClick="btnAdd_Click" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+
+namespace Photo_Resize
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string filename = upFile.FileName ;
+            string path = Path.Combine(HttpContext.Current.Request.MapPath("."), "Photos");
+            
+            // Enregistre la photo originale
+            using var stream = new FileStream(Path.Combine(path, filename), FileMode.Create);
+            upFile.FileContent.CopyTo(stream);
+
+            int width = int.Parse(Width.Text);
+            int height = int.Parse(Height.Text);
+            // Enregistre la photo modifiée
+            ReSize(stream, Path.Combine(path, filename), width, height);
+        }
+        public void ReSize(Stream stream, string adresse, int Width, int Height)
+        {
+            // encode en jpg et en profondeur couleur de 24 bits
+            var myImageCodecInfo = GetEncoderInfo("image/jpeg");
+            var myEncoder = Encoder.ColorDepth;
+            var myEncoderParameters = new EncoderParameters(1);
+            var myEncoderParameter = new EncoderParameter(myEncoder, 24L);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+            // utilise un Bitmap avec les nouvelles dimensions pour recréer l'image en .jpg
+            using Bitmap bitmap = new Bitmap(Width, Height);
+            using Graphics graphic = Graphics.FromImage(bitmap);
+            using (System.Drawing.Image image = System.Drawing.Image.FromStream(stream))
+            {
+                Rectangle rectangle = new Rectangle(0, 0, Width, Height);
+                graphic.DrawImage(image, rectangle);
+            }
+            // copie le fichier redimensionné sur le serveur
+            bitmap.Save(adresse + "_ReSize.jpg", myImageCodecInfo, myEncoderParameters);
+        }
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
+
+    }
+}
